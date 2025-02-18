3bd7488cdb1698a7fa6899f1afeda7a67ec9546e
hazalbuber <91335675+hazalbuber@users.noreply.github.com>
2025-02-18 23:24:07 +0300

    Day 03

---
+++ 10DaysofJavascript/Day1.js
@@ -0,0 +1,13 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    // Print the area of the circle:
+    // Print the perimeter of the circle:
+    const PI = Math.PI
+    const r = parseFloat(readLine());
+    
+    console.log(PI * r ** 2);
+    console.log(2 * PI * r);
+
+
+    //....
+    }
---
+++ 10DaysofJavascript/Day1_1.js
@@ -0,0 +1,7 @@
+function factorial(n) {
+    let result = 1;
+    for( let x = 1; x<=n; x++) {
+           result *=x 
+    }
+    return result;
+} 
